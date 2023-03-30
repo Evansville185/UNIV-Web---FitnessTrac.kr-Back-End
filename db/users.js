57950161673791da4c23c30aec52f3fd8fe648bd
Evansville Rojas <evansville185@gmail.com>
2023-03-30 04:00:29 -0700

    routine functions complete, move onto routine_activities

--- db/users.js
+++ db/users.js
@@ -7,7 +7,7 @@ const SALT_COUNT = 10;
 // user functions
 async function createUser({ username, password }) {
 	const hashedPassword = await bcrypt.hash(password, SALT_COUNT);
-  const userToAdd = {username, hashedPassword}
+	const userToAdd = { username, hashedPassword };
 
 	try {
 		const {
