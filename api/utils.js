8b4f87a2a6715b1c1e93eaea7573a1d1d3a0d24f
Evansville Rojas <evansville185@gmail.com>
2023-04-02 05:28:30 -0700

    continue users functions

--- api/utils.js
+++ api/utils.js
@@ -3,7 +3,7 @@ function requireUser(req, res, next) {
     if(!req.user) {
         next({
             name: "MissingUserError",
-            message: "You must be logged in to perform tihs action",
+            message: "You must be logged in to perform this action",
         });
     }
 
