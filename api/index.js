eb821f3a721dd37bcc4906db3532e1d227c613a0
Evansville Rojas <evansville185@gmail.com>
2023-04-02 05:27:50 -0700

    continue users functions

--- api/index.js
+++ api/index.js
@@ -1,6 +1,49 @@
 const express = require("express");
 const router = express.Router();
 
+//jwt request setup------------------------------------------------------------
+// set `req.user` if necessary
+//might not need - verify
+const jwt = require("jsonwebtoken");
+const { JWT_SECRET } = process.env;
+const { getUserById } = require("../db");
+
+router.use(async (req, res, next) => {
+	const prefix = "Bearer ";
+	const auth = req.header("Authorization");
+
+	if (!auth) {
+		// nothing to see here
+		next();
+	} else if (auth.startsWith(prefix)) {
+		const token = auth.slice(prefix.length);
+
+		try {
+			const { id } = jwt.verify(token, JWT_SECRET);
+
+			if (id) {
+				req.user = await getUserById(id);
+				next();
+			}
+		} catch ({ name, message }) {
+			next({ name, message });
+		}
+	} else {
+		next({
+			name: "AuthorizationHeaderError",
+			message: `Authorization token must start with ${prefix}`,
+		});
+	}
+});
+
+router.use((req, res, next) => {
+	if (req.user) {
+		console.log("User is set:", req.user);
+	}
+
+	next();
+});
+
 // GET /api/health
 router.get("/health", async (req, res, next) => {
 	res.send({
